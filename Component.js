0216224f27151d556d58d58ceb5c6483e95ff860
Daniil <traitnil@gmail.com>
2022-03-01 23:55:49 +0200

    component-require

---
+++ Component.js
@@ -0,0 +1,9 @@
+const num = 2+2;
+
+function mySum(a,b){
+    return a+b;
+}
+
+// sum(num,5);
+
+exports.mySum = mySum;
--- index.js
+++ index.js
@@ -1,8 +1,14 @@
-const lodash = require('lodash');
+const _ = require('lodash');
+const {mySum} = require('./Component.js')
 
-const user = [
+const users = [
     {'user': 'barney', 'age': 36, 'active': true},
     {'user': 'fred', 'age': 40, 'active': false}
 ];
 
+console.log('INDEX FILE START');
 console.log(_.filter(users,'active'));
+console.log(_.random());
+
+console.log(mySum(2,2));
+console.log('INDEX FILE END');
